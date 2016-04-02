13ff1577e7eb964b2de7de66fb43c3dd2ecd705a
cxtadment <cxtadam@gmail.com>
2016-04-01 23:12:24 -0700

    clean all

--- app/views.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('544pj', __name__)
-
-@bp.route('/')
-def index():
-    return "This is our 544 project"
--- config.py
+++
@@ -1,7 +0,0 @@
-MONGODB_SETTINGS = {
-    'DB'       : '544pj',
-    'host'     : '127.0.0.1',
-    'port'     : 27017,
-    'username' : '544admin',
-    'password' : '123456'
-}
